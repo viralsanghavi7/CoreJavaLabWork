50ea8a19fd3e9bb83fecbf6c358f2295529a9b11
Viral Sanghavi <viralsanghavi7@gmail.com>
2016-08-15 15:06:51 -0500

    Addinng test case of incomplete fields

--- src/day7/BuilderMain.java
+++ src/day7/BuilderMain.java
@@ -1,4 +1,5 @@
 package src.day7;
+import src.day7.Target.Builder;
 
 /**
  * Created by vsanghav on 8/12/2016.
@@ -8,14 +9,20 @@ public class BuilderMain {
 
     public static void main(String[] args){
 
-        Target.Builder obj1 = new Target.Builder();
+
+        Builder obj1 = new Builder();
         printMessage(obj1);
 
-        Target.Builder obj2 = new Target.Builder().name("Mr Sad").contact(99887766).age(25).workExperience(35).city("Mexico City");
+        Builder obj2 = new Builder().name("Mr Sad").contact(99887766).age(25).workExperience(35).city("Mexico City");
         printMessage(obj2);
 
-        Target.Builder obj3 = new Target.Builder().name("Mr Happy").contact(99887766).age(25).workExperience(4).city("Adis Ababa");
+        Builder obj3 = new Builder().name("Mr Happy").contact(99887766).age(25).workExperience(4).city("Adis Ababa");
         printMessage(obj3);
+
+        Builder obj4 = new Builder().name("Mr missingField").contact(9845321).workExperience(5);
+        printMessage(obj4);
+
+
 
     }
 
