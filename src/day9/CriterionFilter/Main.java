c4bc2795f8158f4e0f54ef33427251fca5598576
Viral Sanghavi <viralsanghavi7@gmail.com>
2016-08-18 00:08:36 -0500

    Day 10: GeneralFilter7, lambda expressions

--- src/day9/CriterionFilter/Main.java
+++ src/day9/CriterionFilter/Main.java
@@ -11,24 +11,24 @@ import java.util.function.Predicate;
  * Created by vsanghav on 8/16/2016.
  */
 public class Main {
-    
+
+    final static int INTEGER_LIMIT = 5;
+    final static int DAY_LIMIT = 15;
     public static void main(String[] args){
+
 
 
         // --------------- for String ---------------------------//
         List<String> ls = Arrays.asList(
                 "Nitin", "Jim", "William", "Susannah", "Lucy", "Orinoco", "Toby"
         );
-        Predicate palindrome = new Predicate<String>() {
-            @Override
-            public boolean test(String s) {
+        Predicate<String> palindrome = (String s)  -> {
                 s = s.toLowerCase();
                 for(int i=0;i<s.length()/2;i++){
                     if(s.charAt(i)!= s.charAt(s.length()-1-i))
                         return false;
                 }
                 return true;
-            }
         };
         Filter<String> stringFilter = new Filter<>();
         assert stringFilter.filter(ls,palindrome).size() == 1;
@@ -36,14 +36,22 @@ public class Main {
         // --------------- for Integer ---------------------------//
         List<Integer> li = Arrays.asList(5,50,34,67,101);
         Filter<Integer> integerFilter = new Filter<>();
-        Predicate<Integer> greaterThanInteger = new IntegerThreshold(5);
+        Predicate<Integer> greaterThanInteger = (Integer s) -> {
+            if((int)s > INTEGER_LIMIT)
+                return true;
+            return false;
+        };
         assert integerFilter.filter(li,greaterThanInteger).size() == 4;
 
 
         // --------------- for LocalDate ---------------------------//
         List<LocalDate> ld = Arrays.asList(LocalDate.now(), LocalDate.now().minus(5, ChronoUnit.DAYS), LocalDate.now().plus(5,ChronoUnit.DAYS));
         Filter<LocalDate> dateFilter = new Filter<>();
-        Predicate<LocalDate> greaterThanDay = new DateThreshold(15);
+        Predicate<LocalDate> greaterThanDay = (LocalDate s) ->{
+            if(s.getDayOfMonth()<DAY_LIMIT)
+                return true;
+            return false;
+        };
         assert dateFilter.filter(ld,greaterThanDay).size() == 1;
     }
 }
